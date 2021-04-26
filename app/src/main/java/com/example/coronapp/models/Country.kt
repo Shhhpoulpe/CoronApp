263a65776d84c14546562c28a2008f041d928bb5
Shhhpoulpe <max.jobard@hotmail.fr>
2021-04-26 09:52:22 +0200

    dataclass created

--- app/src/main/java/com/example/coronapp/models/Country.kt
+++ app/src/main/java/com/example/coronapp/models/Country.kt
@@ -1,4 +1,15 @@
 package com.example.coronapp.models
 
-class Country {
+data class Country(val name: String,
+                   val iso: String,
+                   val totalCases: Int,
+                   val activeCases: Int,
+                   val deaths: Int,
+                   val recovered: Int,
+                   val critical: Int,
+                   val tested: Int,
+                   val deathRatio: Double,
+                   val recoveryRatio: Double) {
+
+
 }
