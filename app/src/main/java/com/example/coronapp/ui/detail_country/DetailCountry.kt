d2e4af2497a859953e43e2d571f8e5830752266e
Kévin TSI <kevtsiii@gmail.com>
2021-04-26 10:44:50 +0200

    Merge branch 'main' of https://github.com/Shhhpoulpe/CoronApp into main
    # Conflicts:
    #	.gradle/buildOutputCleanup/buildOutputCleanup.lock
    #	.gradle/buildOutputCleanup/outputFiles.bin
    #	.idea/caches/build_file_checksums.ser
    #	.idea/modules/app/CoronApp.app.iml

---
+++ app/src/main/java/com/example/coronapp/ui/detail_country/DetailCountry.kt
@@ -0,0 +1,32 @@
+package com.example.coronapp.ui.detail_country
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.coronapp.R
+
+class DetailCountry : Fragment() {
+
+    companion object {
+        fun newInstance() = DetailCountry()
+    }
+
+    private lateinit var viewModel: DetailCountryViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.detail_country_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DetailCountryViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
