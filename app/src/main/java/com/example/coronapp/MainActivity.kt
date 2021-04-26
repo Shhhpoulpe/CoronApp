1ae460bd547a5e7f69a8662d5cf0aebc82613eac
Shhhpoulpe <max.jobard@hotmail.fr>
2021-04-26 12:19:21 +0200

    Api WIP

--- app/src/main/java/com/example/coronapp/MainActivity.kt
+++ app/src/main/java/com/example/coronapp/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.coronapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.coronapp.api.CountryInterface
+import com.example.coronapp.models.Country
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -11,15 +16,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val BASE_URL = "https://api.quarantine.country"
+        var retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(CountryInterface::class.java)
+        val countryDatas = service.getCountryList()
+
+        countryDatas.enqueue(object: Callback<List<Country>> {
+        override fun onResponse(call: Call<List<Country>>, response: Response<List<Country>>) {
+            val allcountryData = response.body()
+            allcountryData?.let {
+                for( countryData in it) {
+                    Log.d("Country","Country: ${countryData.name}")
+                }
+            }
+        }
+        override fun onFailure(call: Call<List<Country>>, t: Throwable) {
+            Log.e("Country", "Error : $t")
+        }
+    })
     }
 
-    val BASE_URL = "https://api.quarantine.country"
-    var retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-
-    val service = retrofit.create(CountryInterface::class.java)
-    val values = service.getCountryList()
 
 }
